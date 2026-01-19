004ba145c99b3d0a84a92525fbff60c0b87bddd9
Vanderlei Franke Liviz <vanderleifl@hotmail.com>
2026-01-19 17:15:05 -0300

    Estrutura inicial e project group

---
+++ Fase01/Semana01/MeuPrimeiroProjeto.dpr
@@ -0,0 +1,14 @@
+program MeuPrimeiroProjeto;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils;
+
+begin
+   WriteLn('Olá Digital Sistemas!');
+   WriteLn('Tecle <Enter> para finalizar...');
+   ReadLn;
+end.
