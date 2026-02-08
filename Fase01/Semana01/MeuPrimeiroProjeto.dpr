94b6f410eee41f88cdeffeda2a7f800ff3d3bd08
Vanderlei Franke Liviz <vanderleifl@hotmail.com>
2026-02-08 10:05:34 -0300

    Aula 01 finalizada!

--- Fase01/Semana01/MeuPrimeiroProjeto.dpr
+++ Fase01/Semana01/MeuPrimeiroProjeto.dpr
@@ -7,8 +7,22 @@ program MeuPrimeiroProjeto;
 uses
   System.SysUtils;
 
+var
+   nome: string;
+
 begin
+   // Imprimindo mensagens
    WriteLn('Olá Digital Sistemas!');
-   WriteLn('Tecle <Enter> para finalizar...');
+   WriteLn;
+   WriteLn('Qual é o seu nome? (quando terminar, tecle <Enter>)');
+
+   // Lendo o nome do usuario na variavel "nome"
+   ReadLn(nome);
+   WriteLn;
+   WriteLn('Olá ' + nome + '!');
+   WriteLn;
+
+   WriteLn('Tecle <Enter> para finalizar');
+   // Aguardando teclar <Enter>
    ReadLn;
 end.
